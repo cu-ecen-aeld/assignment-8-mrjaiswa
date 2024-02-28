310bacbebd69c7c24940be356dd8cf0fa8597c60
Mrinal Jaiswal <mrinal.jaiswal@colorado.edu>
2024-02-28 16:47:38 -0600

    Added many helper script

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,14 +6,15 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '36ebe918868bdfaed6a8caa0c724b1392ce3e24c'
-#AESD_ASSIGNMENTS_VERSION = 051fead2670c8e61c0b2d5857a0361833be29965
+AESD_ASSIGNMENTS_VERSION = 'f3192081939f063c5e2ed434859b2628a53e5ce3'
+#AESD_ASSIGNMENTS_VERSION = 9833c588062cae449a2f6565a866d3c7866924d3
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-mrjaiswa.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
-
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+#AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver/
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
@@ -30,6 +31,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
+        
+
+
+      
 endef
 
 $(eval $(generic-package))
--- base_external/package/aesd-chardriver/aesd-chardriver.mk
+++ base_external/package/aesd-chardriver/aesd-chardriver.mk
@@ -6,9 +6,10 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHARDRIVER_VERSION = 05fcf0fc594ca6c95e7af802012dfa742ba1b5e3
+AESD_CHARDRIVER_VERSION = 3e0e389a61ceb3fb757be33b382f9ebdf00de383
 AESD_CHARDRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-mrjaiswa.git'
 AESD_CHARDRIVER_SITE_METHOD = git
+AESD_CHARDRIVER_GIT_SUBMODULES = YES
 AESD_CHARDRIVER_MODULE_SUBDIRS = aesd-char-driver/
 
 
@@ -16,7 +17,6 @@ define AESD_CHARDRIVER_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
-
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/assignment-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/sockettest.sh $(TARGET_DIR)/usr/bin
